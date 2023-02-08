8f35186f44db00207865aac86322ed0f208b8ebb
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2023-02-07 21:19:55 -0500

    improve path handling

--- token-refresh/cp-path-to-s3.py
+++ token-refresh/cp-path-to-s3.py
@@ -7,22 +7,25 @@ import os.path
 import yaml
 
 
+config_file = "config.yaml"
+
+
 def get_src_files(src_path):
     src_files = []
     for dirpath, dirnames, filenames in os.walk(Path(src_path)):
         for filename in filenames:
-            src_files.append({"dirpath": dirpath.removeprefix(src_path), "filename": filename})
+            src_files.append({"path": Path(dirpath.removeprefix(src_path)), "filename": filename})
     return src_files
 
 
-def cp_to_s3(filename, key):
+def cp_to_s3(filename, key, config_file=config_file):
     # get the date provider slug
-    data_provider_slug = yaml.safe_load(Path("config.yaml").read_text())["data_provider_slug"]
+    data_provider_slug = yaml.safe_load(Path(config_file).read_text())["data_provider_slug"]
     # set the default profile
     boto3.setup_default_session(profile_name=data_provider_slug)
-    # Set the desired multipart threshold value (1GB)
+    # Set the desired multipart threshold value (4GB)
     GB = 1024**3
-    config = TransferConfig(multipart_threshold=1 * GB)
+    config = TransferConfig(multipart_threshold=GB * 4)
     # Perform the transfer
     s3 = boto3.client("s3")
     s3.upload_file(
@@ -32,11 +35,13 @@ def cp_to_s3(filename, key):
         Config=config,
     )
 
-src_path = "test-files"
-dst_prefix = "test-prefix"
+
+src_path = f"test-files"
+dst_prefix = f"test-prefix/"
 for src_file in get_src_files(src_path):
-    src = os.path.join(src_file["dirpath"], src_file["filename"])
-    dst = dst_prefix + "/" + src_file["dirpath"].replace("\\", "/") + "/" + src_file["filename"]
+    src = os.path.join(src_file["path"], src_file["filename"])
+    src_prefix = Path(*src_file["path"].parts[1:]).as_posix()
+    dst = dst_prefix + src_prefix + "/" + src_file["filename"]
     token_refresh()
     print("Uploading:" + src + " to:" + dst + "...")
     cp_to_s3(src, dst)
